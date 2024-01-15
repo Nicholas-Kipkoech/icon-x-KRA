8a00f127b373a56f71d414cd4302411694a2cb68
Nicholas Kipkoech <nickey968@gmail.com>
2024-01-15 11:56:09 +0300

    add segment modal

--- app/dashboard/admin-etims-ops/AddSegment.jsx
+++ app/dashboard/admin-etims-ops/AddSegment.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { Modal } from "antd";
+import CustomInput from "@/app/ui/reusableComponents/CustomInput";
+import CustomButton from "@/app/ui/reusableComponents/CustomButton";
+const AddSegment = ({ showForm, handleCancel }) => {
+  return (
+    <div>
+      <Modal
+        open={showForm}
+        onCancel={handleCancel}
+        title="Add Segment"
+        centered
+        footer
+      >
+        <CustomInput
+          required
+          name={"Segment Name"}
+          className={"border h-[40px] p-2 rounded-md"}
+        />
+        <CustomInput
+          required
+          name={"Segment Code"}
+          className={"border h-[40px] p-2 rounded-md"}
+        />
 
-const AddSegment = () => {
-  return <div>AddSegment</div>;
+        <div className="flex justify-end mt-[10px] gap-9">
+          <CustomButton
+            name={"Cancel"}
+            onClick={handleCancel}
+            className={"bg-[#e37474] p-2 rounded-md w-[100px] text-white"}
+          />
+          <CustomButton
+            name={"Save"}
+            className={"bg-[#32328f] p-2 rounded-md w-[100px] text-white"}
+          />
+        </div>
+      </Modal>
+    </div>
+  );
 };
 
 export default AddSegment;
--- app/dashboard/admin-etims-ops/segments.jsx
+++ app/dashboard/admin-etims-ops/segments.jsx
@@ -2,10 +2,13 @@
 import React, { useEffect, useState } from "react";
 import { Table } from "antd";
 import { fetchSegments } from "@/app/services/adminServices";
+import CustomButton from "@/app/ui/reusableComponents/CustomButton";
+import AddSegment from "./AddSegment";
 
 const Segments = () => {
   const [segment, setSegment] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [showForm, setShowForm] = useState(false);
 
   const getSegments = async () => {
     setLoading(true);
@@ -32,7 +35,17 @@ const Segments = () => {
   return (
     <div>
       <p className="flex justify-center text-[25px]">Registered Segments</p>
+
       <div className="mt-[10px]">
+        <div className="flex justify-end mr-[10px]">
+          <CustomButton
+            name={"Add Segment"}
+            onClick={() => setShowForm(true)}
+            className={
+              "bg-[#298dc0] text-white p-[5px] rounded-md mb-[10px] hover:bg-[#34c186]"
+            }
+          />
+        </div>
         <Table
           columns={segmentColumns}
           dataSource={segment}
@@ -40,6 +53,7 @@ const Segments = () => {
         />
         ;
       </div>
+      <AddSegment showForm={showForm} handleCancel={() => setShowForm(false)} />
     </div>
   );
 };
