51e06f14ce13f493d9e6799ece67344868a6a50a
Nicholas Kipkoech <nickey968@gmail.com>
2024-01-17 17:41:00 +0300

    completed pending tasks

--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -12,7 +12,6 @@ import {
   MdList,
   MdLogout,
   MdOutlineSettings,
-  MdSupervisedUserCircle,
 } from "react-icons/md";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
