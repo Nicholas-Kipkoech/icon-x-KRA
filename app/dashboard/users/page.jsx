173ff027453f50272bf8a0e121dbe0ffbf254d5f
Nicholas Kipkoech <nickey968@gmail.com>
2024-01-22 11:40:44 +0300

    add fetch users

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,7 +1,60 @@
-import React from "react";
+"use client";
+import { fetchUsers } from "@/app/services/adminServices";
+import React, { useEffect, useState } from "react";
+import { Table } from "antd";
 
 const Users = () => {
-  return <div>Users page</div>;
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const getUsers = async () => {
+    try {
+      setLoading(true);
+      const { _users } = await fetchUsers();
+      const filteredUsers = _users.filter((user) => user.role !== "Superadmin");
+      setUsers(filteredUsers);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Business Segment",
+      dataIndex: "business_segment",
+      key: "business_segment",
+    },
+    {
+      title: "Business Family",
+      dataIndex: "business_family",
+      key: "business_family",
+    },
+    {
+      title: "Business Class",
+      dataIndex: "business_class",
+      key: "business_class",
+    },
+  ];
+
+  return (
+    <div className="mt-5">
+      <Table columns={columns} dataSource={users} loading={loading} />
+    </div>
+  );
 };
 
 export default Users;
